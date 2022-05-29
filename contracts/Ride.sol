5dfcc2f568a63717d5393327363913de5fd10c77
Harshit Bhargava <harshit.bhargava@ttu.edu>
2022-05-29 19:20:23 +0530

    Updated Ride.sol

--- contracts/Ride.sol
+++ contracts/Ride.sol
@@ -2,4 +2,27 @@ pragma solidity ^0.5.0;
 
 contract Ride {
     uint256 public rideNo = 1;
+
+    struct Rider {
+        uint256 id;
+        string name;
+        uint256 age;
+        Route route;
+    }
+
+    struct Driver {
+        uint256 id;
+        string name;
+        string status;
+        string car;
+        mapping(uint256 => Route) routes;
+    }
+
+    struct Route {
+        uint256 id;
+        string origin;
+        string destination;
+        string estimatedTimeDep;
+        string estimatedTimeArr;
+    }
 }
